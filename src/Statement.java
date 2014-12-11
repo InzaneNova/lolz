f49fbd33d4a8d577f686db1c1f73662c308663de
Ole M. S <strohm99@gmail.com>
2014-12-11 22:47:53 +0100

    Added support for parentheses

--- src/Statement.java
+++ src/Statement.java
@@ -12,17 +12,28 @@ public class Statement {
 	private static final String OPERATORS2 = "*/";
 	private static final String OPERATORS = OPERATORS1 + OPERATORS2;
 	private static final String VARIABLES = "xywh";
-	private static final String ACCEPTED_CHARACTERS = NUMBERS + OPERATORS + VARIABLES;
+	private static final String OTHER = "()";
+	private static final String ACCEPTED_CHARACTERS = NUMBERS + OPERATORS + VARIABLES + OTHER;
 
 	public Statement(String stmt) {
 		STMT = fullTrim(stmt);
 	}
 
 	public float parseStatement(int x, int y, int w, int h) {
-		List<String> terms = getTerms(STMT);
+		return parseStatement(STMT, x, y, w, h);
+	}
+
+	public float parseStatement(String stmt, int x, int y, int w, int h) {
+		List<String> terms = getTerms(stmt);
 
 		float result = 0;
 
+		for(int p = 0; p < terms.size(); p++) {
+			if(terms.get(p).charAt(0) == '(') {
+				String term = terms.get(p);
+				terms.set(p, "" + parseStatement(term.substring(1, term.length() - 1), x, y, w, h));
+			}
+		}
 
 		for (int pos = 0; pos < terms.size(); pos++) {
 			if (terms.get(pos).charAt(0) == 'x') terms.set(pos, "" + x);
@@ -33,7 +44,8 @@ public class Statement {
 
 		for (int i = 0; i < terms.size(); i++) {
 			boolean isNextImportant = false;
-			if(i < terms.size() - 2) isNextImportant = (terms.get(i + 2).charAt(0) == '*') || (terms.get(i + 2).charAt(0) == '*');
+			if (i < terms.size() - 2)
+				isNextImportant = (terms.get(i + 2).charAt(0) == '*') || (terms.get(i + 2).charAt(0) == '*');
 			char operator = terms.get(i).charAt(0);
 			if (contains(OPERATORS2, operator)) {
 				float a = Float.parseFloat(terms.get(i - 1));
@@ -80,6 +92,7 @@ public class Statement {
 		List<String> terms = new ArrayList<String>();
 
 		boolean num = false;
+		boolean insideParentheses = false;
 
 		int pos = 0;
 		int termPos = 0;
@@ -87,35 +100,45 @@ public class Statement {
 		for (int i = 0; i < stmt.length(); i++) {
 			char c = stmt.charAt(i);
 
-			if (contains(VARIABLES, c)) {
-				if (contains(NUMBERS, terms.get(termPos - 1).charAt(0)) || contains(VARIABLES, terms.get(termPos - 1).charAt(0))) {
-					terms.add("*");
+			if (!insideParentheses) {
+				if (contains(VARIABLES, c)) {
+					if (contains(NUMBERS, terms.get(termPos - 1).charAt(0)) || contains(VARIABLES, terms.get(termPos - 1).charAt(0))) {
+						terms.add("*");
+						termPos++;
+					}
+					terms.add("" + c);
 					termPos++;
-				}
-				terms.add("" + c);
-				termPos++;
 
-			} else if (contains(NUMBERS, c)) {
-				if (!num) {
-					num = true;
+				} else if (contains(NUMBERS, c)) {
+					if (!num) {
+						num = true;
+						pos = i;
+					}
+					if (i == stmt.length() - 1 || !contains(NUMBERS, stmt.charAt(i + 1))) {
+						num = false;
+						terms.add(stmt.substring(pos, i + 1));
+						termPos++;
+					}
+
+				} else if(contains(OPERATORS, c)) {
+					terms.add("" + c);
+					termPos++;
+				} else if(c == '('){
 					pos = i;
+					insideParentheses = true;
 				}
-				if (i == stmt.length() - 1 || !contains(NUMBERS, stmt.charAt(i + 1))) {
-					num = false;
-					terms.add(stmt.substring(pos, i + 1));
-					termPos++;
-				}
-
 			} else {
-				terms.add("" + c);
-				termPos++;
+				if(c == ')') {
+					terms.add(stmt.substring(pos, i+1));
+					insideParentheses = false;
+				}
 			}
 		}
 		return terms;
 	}
 
 	public static boolean isValidStmt(String stmt) {
-		if(stmt.length() == 0) {
+		if (stmt.length() == 0) {
 			PixelArt.output("An empty function is not valid.", "ERROR");
 			return false;
 		}
