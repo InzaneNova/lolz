1fd3a5fb2c284bf14974903695ed3608016ca415
Ole M. S <strohm99@gmail.com>
2014-12-11 21:49:37 +0100

    Setup commit

---
+++ src/PixelArt.java
@@ -0,0 +1,178 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+
+/**
+ * Created by Ole on 25/11/2014.
+ */
+public class PixelArt extends Canvas implements Runnable {
+
+	private static final int WIDTH = 720;
+	private static final int HEIGHT = (WIDTH / 16) * 9;
+	private static final Dimension d = new Dimension(WIDTH, HEIGHT);
+
+	private JFrame frame;
+	private boolean running = true;
+	private BufferedImage img = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+	private int[] pixels = ((DataBufferInt) img.getRaster().getDataBuffer()).getData();
+
+	public static void main(String[] args) {
+		PixelArt pa = new PixelArt();
+		pa.start();
+	}
+
+	private void start() {
+		setMinimumSize(d);
+		setMaximumSize(d);
+		setPreferredSize(d);
+
+		frame = new JFrame("lol");
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.setResizable(true);
+		frame.add(this);
+		frame.pack();
+		frame.setVisible(true);
+
+		new Thread(this, "main").start();
+	}
+
+	public void run() {
+		while (running) {
+//			Statement stmt = new Statement(inputString("skriv en funksjon med x og y"));
+//			int xtest = inputInt("input a number, x");
+//			int ytest = inputInt("input a number, y");
+//			float i = stmt.parseStatement(xtest, ytest, WIDTH, HEIGHT);
+//			output("At x = " + xtest+  " and y = " + ytest + ":   f(x, y) = " + stmt.getStatement() + " = " + i, "test");
+
+			Statement stmtr = new Statement(inputString("skriv en funksjon med x, y, w, h"));
+			Statement stmtg = new Statement(inputString("skriv en funksjon med x, y, w, h"));
+			Statement stmtb = new Statement(inputString("skriv en funksjon med x, y, w, h"));
+
+			int r, g, b;
+
+			for (int y = 0; y < HEIGHT; y++) {
+				for (int x = 0; x < WIDTH; x++) {
+//					double xy1 = (x * y) / ((double) WIDTH * (double) HEIGHT);
+//					double xy2 = ((WIDTH - x) * y) / ((double) WIDTH * (double) HEIGHT);
+//					double xy3 = (x * (HEIGHT - y)) / ((double) WIDTH * (double) HEIGHT);
+//					double cool = ((WIDTH - x) * (HEIGHT - y)) / ((double) WIDTH * (double) HEIGHT);
+//					r = (int) ((xy1 * 255) - cool * 255);
+//					g = (int) ((xy2 * 255) - cool * 255);
+//					b = (int) ((xy3 * 255) - cool * 255);
+
+					r = (int) stmtr.parseStatement(x, y, WIDTH, HEIGHT);
+					g = (int) stmtg.parseStatement(x, y, WIDTH, HEIGHT);
+					b = (int) stmtb.parseStatement(x, y, WIDTH, HEIGHT);
+
+					pixels[x + y * WIDTH] = (r << 16) + (g << 8) + b;
+				}
+			}
+
+			render();
+
+			output("red = " + stmtr.getStatement() +
+					"\ngreen = " + stmtg.getStatement() +
+					"\nblue = " + stmtb.getStatement(),
+					"Info");
+		}
+	}
+
+	public void render() {
+		BufferStrategy bs = getBufferStrategy();
+		if (bs == null) {
+			createBufferStrategy(2);
+			bs = getBufferStrategy();
+		}
+
+		Graphics g = bs.getDrawGraphics();
+
+		g.drawImage(img, 0, 0, WIDTH, HEIGHT, null);
+
+		g.dispose();
+		bs.show();
+	}
+
+	public static int inputInt(String msg, int min, int max) {
+		boolean done = false;
+		int faults = 0;
+
+		int input = 0;
+		while (!done) {
+			try {
+				String in = inputString(msg);
+				input = Integer.parseInt(in);
+				done = true;
+				if (input < min || input > max) {
+					output("Tallet må være mellom " + min + "-" + max, "Feil");
+					done = false;
+				}
+			} catch (Exception e) {
+				if (faults < 2) {
+					faults++;
+					output("Du må skrive inn et tall. ;)", "feil");
+				} else {
+					output("DU MÅ SKRIVE INN ET TALL!!!!! >:(", "feil");
+				}
+			}
+		}
+		return input;
+	}
+
+	public static int inputInt(String msg) {
+		boolean done = false;
+		int faults = 0;
+
+		int input = 0;
+		while (!done) {
+			try {
+				String in = inputString(msg);
+				input = Integer.parseInt(in);
+				done = true;
+			} catch (Exception e) {
+				if (faults < 2) {
+					faults++;
+					output("Du må skrive inn et tall. ;)", "feil");
+				} else {
+					output("DU MÅ SKRIVE INN ET TALL!!!!! >:(", "feil");
+				}
+			}
+		}
+		return input;
+	}
+
+	public static int inputYN(String msg, String title) {
+		return JOptionPane.showConfirmDialog(null,
+				msg != null
+						? msg
+						: "",
+				title != null
+						? title
+						: "",
+				JOptionPane.YES_NO_OPTION);
+	}
+
+	public static String inputString(String msg) {
+		boolean done = false;
+
+		String input = "";
+
+		while (!done) {
+			input = JOptionPane.showInputDialog(msg);
+			if (input != null) {
+				done = true;
+			} else {
+				if (inputYN("Er du sikker på at du vil avslutte?", "Avslutte?") == 0) System.exit(0);
+				output("Bra du vil bli. :)", ":D");
+			}
+		}
+		return input;
+	}
+
+	public static void output(String msg, String title) {
+		if (msg != null && title != null)
+			JOptionPane.showMessageDialog(null, msg, title, JOptionPane.INFORMATION_MESSAGE);
+	}
+
+}
---
+++ src/Statement.java
@@ -0,0 +1,146 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Ole on 04/12/2014.
+ */
+public class Statement {
+
+	private final String STMT;
+	private static final String NUMBERS = " 0123456789.";
+	private static final String OPERATORS1 = "+-";
+	private static final String OPERATORS2 = "*/";
+	private static final String OPERATORS = OPERATORS1 + OPERATORS2;
+	private static final String VARIABLES = "xywh";
+	private static final String ACCEPTED_CHARACTERS = NUMBERS + OPERATORS + VARIABLES;
+
+	public Statement(String stmt) {
+		STMT = fullTrim(stmt);
+	}
+
+	public float parseStatement(int x, int y, int w, int h) {
+		List<String> terms = getTerms(STMT);
+
+		float result = 0;
+
+
+		for (int pos = 0; pos < terms.size(); pos++) {
+			if (terms.get(pos).charAt(0) == 'x') terms.set(pos, "" + x);
+			else if (terms.get(pos).charAt(0) == 'y') terms.set(pos, "" + y);
+			else if (terms.get(pos).charAt(0) == 'w') terms.set(pos, "" + w);
+			else if (terms.get(pos).charAt(0) == 'h') terms.set(pos, "" + h);
+		}
+
+		for (int i = 0; i < terms.size(); i++) {
+			boolean isNextImportant = false;
+			if(i < terms.size() - 2) isNextImportant = (terms.get(i + 2).charAt(0) == '*') || (terms.get(i + 2).charAt(0) == '*');
+			char operator = terms.get(i).charAt(0);
+			if (contains(OPERATORS2, operator)) {
+				float a = Float.parseFloat(terms.get(i - 1));
+				float b = Float.parseFloat(terms.get(i + 1));
+
+				float temp_res = 0;
+
+				if (operator == '*') {
+					temp_res = a * b;
+				} else if (operator == '/') {
+					b = (b != 0 ? b : Float.MIN_VALUE);
+					temp_res = a / b;
+				}
+				terms.remove(i + 1);
+				terms.remove(i);
+				terms.set(i - 1, "" + temp_res);
+
+				if (isNextImportant) i -= 2;
+			}
+		}
+
+		for (int i = 1; i < terms.size(); i += 2) {
+			char operator = terms.get(i - 1).charAt(0);
+			if (operator == '+') {
+				result += Float.parseFloat(terms.get(i));
+			} else if (operator == '-') {
+				result -= Float.parseFloat(terms.get(i));
+
+			} else if (operator == '*') {
+				result *= Float.parseFloat(terms.get(i));
+
+			} else if (operator == '/') {
+				result /= Float.parseFloat(terms.get(i));
+
+			}
+		}
+
+		return result;
+	}
+
+	private List<String> getTerms(String stmt) {
+		if (!contains(OPERATORS, stmt.charAt(0))) stmt = "+" + stmt;
+		stmt = fullTrim(stmt);
+		List<String> terms = new ArrayList<String>();
+
+		boolean num = false;
+
+		int pos = 0;
+		int termPos = 0;
+
+		for (int i = 0; i < stmt.length(); i++) {
+			char c = stmt.charAt(i);
+
+			if (contains(VARIABLES, c)) {
+				if (contains(NUMBERS, terms.get(termPos - 1).charAt(0)) || contains(VARIABLES, terms.get(termPos - 1).charAt(0))) {
+					terms.add("*");
+					termPos++;
+				}
+				terms.add("" + c);
+				termPos++;
+
+			} else if (contains(NUMBERS, c)) {
+				if (!num) {
+					num = true;
+					pos = i;
+				}
+				if (i == stmt.length() - 1 || !contains(NUMBERS, stmt.charAt(i + 1))) {
+					num = false;
+					terms.add(stmt.substring(pos, i + 1));
+					termPos++;
+				}
+
+			} else {
+				terms.add("" + c);
+				termPos++;
+			}
+		}
+		return terms;
+	}
+
+	public static boolean isValidStmt(String stmt) {
+		for (int i = 0; i < stmt.length(); i++) {
+			if (ACCEPTED_CHARACTERS.indexOf(stmt.charAt(i)) < 0) {
+				PixelArt.output("The statement is not valid.", "ERROR");
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static String fullTrim(String str) { // returns str without any spaces
+		String result = "";
+		for (int c = 0; c < str.length(); c++) {
+			char charAt = str.charAt(c);
+			if (charAt != ' ') result += charAt;
+		}
+		return result;
+	}
+
+	public static boolean contains(String str, char c) {
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == c) return true;
+		}
+		return false;
+	}
+
+	public String getStatement() {
+		return STMT;
+	}
+}
